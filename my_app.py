6d674b877225fbf6012e89e406b44920fe369d36
VladSkopenko <skopirka2k17@gmail.com>
2024-11-24 15:32:12 +0200

    update readme

--- my_app.py
+++ my_app.py
@@ -1,46 +1,16 @@
 import streamlit as st
 from PIL import Image
-import numpy as np
 from load_model import model_conv
 from utils import make_prediction
 from utils import preprocess_image
 
-#st.title("Візуалізація роботи нейронної мережі")
-
-#uploaded_image = st.file_uploader("Завантажте зображення", type=["jpg", "png"])
-
-#model_choice = st.selectbox("Оберіть модель", ["Згорткова модель", "VGG16"])
-
-# if st.button("Класифікувати"):
-#     if uploaded_image:
-#
-#         image = Image.open(uploaded_image)
-#         st.image(image, caption="Вхідне зображення")
-#
-#         img_array = preprocess_image(image)
-#
-#
-#         try:
-#             model = model_conv
-#         except ValueError as e:
-#             st.error(str(e))
-#             model = None
-#
-#         if model:
-#             predictions = model.predict(img_array)
-#             predicted_class = np.argmax(predictions)
-#             st.write(f"Передбачений клас: {predicted_class}")  # Тут ще руками потрібно буде обробити , щоб не просто класс 9 а наприклад - автомобіль
-#             st.write(f"Ймовірності: {predictions}")
-#
-#     else:
-#         st.warning("Будь ласка, завантажте зображення.")
 
 def main():
     st.title("Візуалізація роботи нейронної мережі")
 
     uploaded_image = st.file_uploader("Завантажте зображення", type=["jpg", "png"])
 
-    model_choice = st.selectbox("Оберіть модель", ["Згорткова модель", "VGG16"])
+    model_choice = st.selectbox("Оберіть модель", ["Згорткова модель", "VGG16", "Умовна модель №3"])# Ось це впливає на кнопку з вибором моделі
     confidence_threshold = st.slider("Оберіть поріг впевненості", min_value=0.0, max_value=1.0, value=0.70, step=0.01)
 
     if st.button("Класифікувати"):
@@ -51,7 +21,7 @@ def main():
             img_array = preprocess_image(image)
 
             try:
-                model = model_conv
+                model = model_conv # Ось тут ви можете розширити кількість моделей, додати функцію можливо яка буде обирати модель
             except ValueError as e:
                 st.error(str(e))
                 model = None
---
+++ readme.md
@@ -0,0 +1,17 @@
+Для запуска:  
+Завантаження
+```bash 
+git clone https://github.com//VladSkopenko/Example_for_student.git
+```
+Переходимо в теку з застосунком 
+```
+cd /VladSkopenko/Example_for_student
+```
+Встановлюємо залежності
+```
+pip install -r requirements.txt
+```
+Запускаємо через streamlit
+```
+streamlit run app.py
+```
